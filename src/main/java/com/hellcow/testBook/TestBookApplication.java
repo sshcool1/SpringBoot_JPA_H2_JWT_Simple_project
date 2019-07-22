412833de53afbd29f8e684f38d1d5752934dd356
지옥소고기 <>
2019-07-22 15:45:38 +0900

    test

---
+++ src/main/java/com/hellcow/testBook/TestBookApplication.java
@@ -0,0 +1,13 @@
+package com.hellcow.testBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBookApplication.class, args);
+	}
+
+}
